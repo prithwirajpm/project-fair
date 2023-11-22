bdd2c9452223acb1ae7f10072194e1f949adc573
prithwirajpm <wt.prithwiraj@gmail.com>
2023-11-22 05:42:22 +0530

    set view More Project issue

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -13,7 +13,7 @@ function ProjectCard({ project }) {
         <>
             {project &&
                 <Card className='shadow mb-5 btn' onClick={handleShow}>
-                    <Card.Img variant="top" src={project?`${BASE_URL}/uploads/${project.projectImage}`:ProjectOne} />
+                    <Card.Img variant="top" src={project?`${BASE_URL}/uploads/${project?.projectImage}`:ProjectOne} />
                     <Card.Body>
                         <Card.Title>{project.title}</Card.Title>
                     </Card.Body>
@@ -22,7 +22,7 @@ function ProjectCard({ project }) {
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>{project.title}</Modal.Title>
+                    <Modal.Title>{project?.title}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Row>
@@ -30,14 +30,14 @@ function ProjectCard({ project }) {
                             <img style={{ height: '200px' }} className='img-fluid' src={project?`${BASE_URL}/uploads/${project.projectImage}`:ProjectOne} />
                         </Col>
                         <Col md={6}>
-                            <h2>{project.title}</h2>
-                            <p>Project Overview:  <span className='fw-bolder'>{project.overview}</span> </p>
-                            <p>Language Used : <span className='fw-bolder'>{project.language}</span></p>
+                            <h2>{project?.title}</h2>
+                            <p>Project Overview:  <span className='fw-bolder'>{project?.overview}</span> </p>
+                            <p>Language Used : <span className='fw-bolder'>{project?.language}</span></p>
                         </Col>
                     </Row>
                     <div className='mt-3'>
-                        <a href={project.github} target='_blank' className='me-3 btn'><i className='fa-brands fa-github fa-2x'></i></a>
-                        <a href={project.website} target='_blank' className='me-3 btn'><i className='fa-solid fa-link fa-2x'></i></a>
+                        <a href={project?.github} target='_blank' className='me-3 btn'><i className='fa-brands fa-github fa-2x'></i></a>
+                        <a href={project?.website} target='_blank' className='me-3 btn'><i className='fa-solid fa-link fa-2x'></i></a>
 
                     </div>
                 </Modal.Body>
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,25 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../Components/Header'
-import {Form,Row,Col} from 'react-bootstrap';
+import { Form, Row, Col } from 'react-bootstrap';
 import ProjectCard from '../Components/ProjectCard';
+import { allProjectsAPI } from '../Services/allAPI';
 
 function Projects() {
+  const [allProjects, setallProjects] = useState()
+
+  const getallProjects = async () => {
+    if (sessionStorage.getItem("token")) {
+      const token = sessionStorage.getItem("token")
+      const reqHeader = {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      }
+      const result = await allProjectsAPI(reqHeader)
+      if (result.status == 200) {
+        setallProjects(result.data)
+      } else {
+        console.log(result)
+      }
+    }
+  }
+
+  useEffect(() => {
+    getallProjects()
+  }, [])
   return (
     <div>
       <Header />
-      <div style={{marginTop:'100px'}} className='projects'>
+      <div style={{ marginTop: '100px' }} className='projects'>
         <h1 className='text-center mb-5'>All Projects</h1>
         <div className='d-flex justify-content-center align-items-center w-100'>
           <div className='d-flex border w-50 rounded'>
-            <input type='text' className='form-control' placeholder='search Project its technolagy '/>
-            <i style={{marginLeft:'-50px'}} class="fa-solid fa-magnifying-glass fa-rotate-90"></i>
+            <input type='text' className='form-control' placeholder='search Project its technolagy ' />
+            <i style={{ marginLeft: '-50px' }} class="fa-solid fa-magnifying-glass fa-rotate-90"></i>
           </div>
         </div>
       </div>
       <Row className='mt-5 container-fluid'>
-        <Col sm={12} md={6} lg={4}>
-          <ProjectCard />
-        </Col>
+        {allProjects?.length > 0 ? allProjects?.map(project => (
+          <Col sm={12} md={6} lg={4}>
+            <ProjectCard project={project} />
+          </Col>
+        )) : null}
+
       </Row>
     </div>
   )
